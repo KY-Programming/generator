4641a90083ae3773f6e4969d2cf3b56d6c46a1c8
KY <kai@ky-programming.de>
2018-04-28 11:36:42 +0200

    Console initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using KY.Generator.AspDotNet;
+using KY.Generator.OData;
+using KY.Generator.Reflection;
+using KY.Generator.Tsql;
+
+namespace KY.Generator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool success = Generator.Initialize()
+                                    .PreloadModule<AspDotNetModule>()
+                                    .PreloadModule<CsharpModule>()
+                                    .PreloadModule<TypeScriptModule>()
+                                    .PreloadModule<TsqlModule>()
+                                    .PreloadModule<ODataModule>()
+                                    .PreloadModule<ReflectionModule>()
+                                    .SetOutput(args.Skip(1).FirstOrDefault())
+                                    .ReadConfiguration(args.FirstOrDefault())
+                                    .Run();
+            if (!success)
+            {
+                Environment.ExitCode = 1;
+            }
+
+#if DEBUG
+            Console.WriteLine("Press key to EXIT...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
