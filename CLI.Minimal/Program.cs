8856badda84dee2f8b2612e8a1c54acc357be3a1
KY <kai@ky-programming.de>
2019-10-07 16:37:48 +0200

    Preparations for v2

---
+++ CLI.Minimal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using KY.Core;
+using KY.Generator.Watchdog;
+
+namespace KY.Generator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Logger.AllTargets.Add(Logger.VisualStudioOutput);
+
+            bool success = Generator.Initialize()
+                                    .PreloadModule<WatchdogModule>()
+                                    .SetParameters(args)
+                                    .Run();
+            if (!success)
+            {
+                Environment.ExitCode = 1;
+            }
+
+#if DEBUG
+            Console.WriteLine("Press key to EXIT...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ CLI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using KY.Core;
+using KY.Generator.Angular;
+using KY.Generator.AspDotNet;
+using KY.Generator.Csharp;
+using KY.Generator.Json;
+using KY.Generator.OData;
+using KY.Generator.OData.Extensions;
+using KY.Generator.Reflection;
+using KY.Generator.Tsql;
+using KY.Generator.Tsql.Extensions;
+using KY.Generator.TypeScript;
+using KY.Generator.Watchdog;
+
+namespace KY.Generator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Logger.AllTargets.Add(Logger.VisualStudioOutput);
+
+            bool success = Generator.Initialize()
+                                    .PreloadModule<AspDotNetModule>()
+                                    .PreloadModule<CsharpModule>()
+                                    .PreloadModule<TypeScriptModule>()
+                                    .PreloadModule<TsqlModule>()
+                                    .PreloadModule<ODataModule>()
+                                    .PreloadModule<ReflectionModule>()
+                                    .PreloadModule<AngularModule>()
+                                    .PreloadModule<JsonModule>()
+                                    .PreloadModule<WatchdogModule>()
+                                    .SetParameters(args)
+                                    .Run();
+            if (!success)
+            {
+                Environment.ExitCode = 1;
+            }
+
+#if DEBUG
+            Console.WriteLine("Press key to EXIT...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
