341e05c504c0b13a4995264c8fc406d35cbadf59
KY <kai@ky-programming.de>
2020-09-29 14:28:25 +0200

    Examples and tests update to latest version

--- Tests/build.cmd
+++ Tests/build.cmd
@@ -10,11 +10,31 @@ dotnet build --no-incremental
 cd ..
 
 REM ==========================================
-REM ||      WebApiControllerWithRoute      ||
+REM ||               Types                 ||
 REM ==========================================
 REM 
-cd WebApiControllerWithRoute
+cd Types
 rmdir /S/Q Output
+dotnet add package KY.Generator
+dotnet add package KY.Generator.Reflection.Annotations
+dotnet build --no-incremental
+cd ..
+
+REM ==========================================
+REM ||           WebApiController           ||
+REM ==========================================
+REM 
+cd WebApiController
+rmdir /S/Q ClientApp\src\app\fix-casing\models
+rmdir /S/Q ClientApp\src\app\fix-casing\services
+rmdir /S/Q ClientApp\src\app\keep-casing\models
+rmdir /S/Q ClientApp\src\app\keep-casing\services
+rmdir /S/Q ClientApp\src\app\produces\models
+rmdir /S/Q ClientApp\src\app\produces\services
+rmdir /S/Q ClientApp\src\app\routed\models
+rmdir /S/Q ClientApp\src\app\routed\services
+rmdir /S/Q ClientApp\src\app\versioned-api\models
+rmdir /S/Q ClientApp\src\app\versioned-api\services
 dotnet add package KY.Generator
 dotnet add package KY.Generator.Reflection.Annotations
 dotnet build --no-incremental
