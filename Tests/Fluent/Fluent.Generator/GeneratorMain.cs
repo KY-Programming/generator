2dd60d91bcd90a392ddef6f5b656939c0a66f398
KY <kai@ky-programming.de>
2020-12-14 17:08:44 +0100

    Tests and examples updated

---
+++ Tests/Fluent/Fluent.Generator/GeneratorMain.cs
@@ -0,0 +1,16 @@
+﻿using KY.Generator;
+
+namespace Fluent.Generator
+{
+    public class GeneratorMain : GeneratorFluentMain
+    {
+        public override void Execute()
+        {
+            this.Read()
+                .FromType<Types.Types>()
+                .Write()
+                .AngularModels().OutputPath("Output/Models").SkipHeader()
+                .AngularServices().OutputPath("Output/Services").SkipHeader();
+        }
+    }
+}
---
+++ Tests/Fluent/Fluent.Generator/Output/Models/sub-type.ts
@@ -0,0 +1,11 @@
+﻿// tslint:disable
+
+export class SubType {
+    public property: string;
+
+    public constructor(init: Partial<SubType> = undefined) {
+        Object.assign(this, init);
+    }
+}
+
+// outputid:cabb1031-51d3-4479-98b6-c8c0321d279a
