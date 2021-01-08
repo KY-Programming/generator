b4e6ae2b1a32139c16a9e89becc48ff6c394df5f
KY <kai@ky-programming.de>
2021-01-08 12:38:17 +0100

    - allow ignore attribute on structs

--- Annotations/Reflection/GenerateIgnoreAttribute.cs
+++ Annotations/Reflection/GenerateIgnoreAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace KY.Generator
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method, Inherited = false)]
     public class GenerateIgnoreAttribute : Attribute
     { }
 }
