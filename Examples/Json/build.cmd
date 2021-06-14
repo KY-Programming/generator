accd72a53ef765400998d18be11e1e158aa2fdba
KY <kai@ky-programming.de>
2021-06-14 15:31:08 +0200

    Examples updated

--- Examples/Json/build.cmd
+++ Examples/Json/build.cmd
@@ -5,7 +5,6 @@ REM
 cd JsonWithReader
 rmdir /S/Q bin
 rmdir /S/Q Output
-dotnet add package KY.Generator --prerelease
 dotnet add package KY.Generator.Angular --prerelease
 dotnet add package KY.Generator.Fluent --prerelease
 dotnet add package KY.Generator.Json --prerelease
