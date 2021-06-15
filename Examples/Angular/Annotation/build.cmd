b1191c68c8f9cad71cb082f6751eb40a3b8213df
KY <kai@ky-programming.de>
2021-06-15 15:42:26 +0200

    Angular  - examples restructured

---
+++ Examples/Angular/Annotation/build.cmd
@@ -0,0 +1,39 @@
+REM 
+REM ==========================================
+REM ||          ModelFromAssembly           ||
+REM ==========================================
+REM 
+cd ModelFromAssembly
+rmdir /S/Q bin
+rmdir /S/Q Output
+dotnet add package KY.Generator --prerelease
+dotnet add package KY.Generator.Annotations --prerelease
+dotnet build --no-incremental
+cd ..
+
+REM ==========================================
+REM ||    ServiceFromAspNetCore    ||
+REM ==========================================
+REM 
+cd ServiceFromAspNetCore
+rmdir /S/Q bin
+rmdir /S/Q ClientApp\src\app\models
+rmdir /S/Q ClientApp\src\app\services
+dotnet add package KY.Generator --prerelease
+dotnet add package KY.Generator.Annotations --prerelease
+dotnet build --no-incremental
+cd ..
+
+REM 
+REM ==========================================
+REM ||    ServiceFromAspNetCoreSignalRHub    ||
+REM ==========================================
+REM 
+cd ServiceFromAspNetCoreSignalRHub
+rmdir /S/Q bin
+rmdir /S/Q ClientApp\src\app\models
+rmdir /S/Q ClientApp\src\app\services
+dotnet add package KY.Generator --prerelease
+dotnet add package KY.Generator.Annotations --prerelease
+dotnet build --no-incremental
+cd ..
---
+++ Examples/Angular/Fluent/build.cmd
@@ -0,0 +1,34 @@
+REM 
+REM ==========================================
+REM ||        ServiceFromAspNetCore         ||
+REM ==========================================
+REM 
+cd ServiceFromAspNetCore
+rmdir /S/Q bin
+rmdir /S/Q Service\ClientApp\src\app\models
+rmdir /S/Q Service\ClientApp\src\app\services
+cd Generator
+dotnet add package KY.Generator --prerelease
+dotnet add package KY.Generator.Fluent --prerelease
+dotnet add package KY.Generator.Angular --prerelease
+dotnet add package KY.Generator.AspDotNet --prerelease
+dotnet add package KY.Generator.Reflection --prerelease
+cd ..\..
+dotnet build ServiceFromAspNetCore.sln --no-incremental
+
+REM 
+REM ==========================================
+REM ||         ServiceFromSignalR           ||
+REM ==========================================
+REM 
+cd ServiceFromSignalR
+rmdir /S/Q bin
+rmdir /S/Q Service\ClientApp\src\app\models
+rmdir /S/Q Service\ClientApp\src\app\services
+cd Generator
+dotnet add package KY.Generator --prerelease
+dotnet add package KY.Generator.Fluent --prerelease
+dotnet add package KY.Generator.Angular --prerelease
+dotnet add package KY.Generator.Reflection --prerelease
+cd ..\..
+dotnet build ServiceFromSignalR.sln --no-incremental
