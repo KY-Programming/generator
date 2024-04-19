801b203176c2965e8bb399cc72f3a12344833daa
KY <kai@ky-programming.de>
2024-04-19 14:43:12 +0200

    Examples updated

---
+++ Tests/Types/Output/Enums/starting-index-enum.ts
@@ -0,0 +1,15 @@
+﻿/* eslint-disable */
+// tslint:disable
+
+export enum StartingIndexEnum {
+    value1 = 1,
+    value2 = 2,
+    value3 = 3
+}
+
+export const StartingIndexEnumValues = [1, 2, 3];
+export const StartingIndexEnumNames = ["Value1", "Value2", "Value3"];
+export const StartingIndexEnumValueMapping: { [key: number]: string } = { 1: "Value1", 2: "Value2", 3: "Value3" };
+export const StartingIndexEnumNameMapping: { [key: string]: number } = { "Value1": 1, "Value2": 2, "Value3": 3 };
+
+// outputid:605e91d7-ee13-4f1d-9b92-845bb3ace852
