e5cf85a9f4edd3e6bfe9a2bf9a9037911bee0d98
KY <kai@ky-programming.de>
2025-07-13 10:05:05 +0200

    feat: improve v10 tests

--- Tests/v10/TypeScriptValidationStrict/index.ts
+++ Tests/v10/TypeScriptValidationStrict/index.ts
@@ -1,37 +1,37 @@
 import { Class1 } from '../CustomBuildOutputLocation/Output/class-1';
 
-import { ByteEnum } from '../EnumAnnotationsNotNullable/Output/byte-enum';
-import { DefaultEnum } from '../EnumAnnotationsNotNullable/Output/default-enum';
-import { IndexedEnum } from '../EnumAnnotationsNotNullable/Output/indexed-enum';
-import { IntEnum } from '../EnumAnnotationsNotNullable/Output/int-enum';
-import { LongEnum } from '../EnumAnnotationsNotNullable/Output/long-enum';
-import { NegativeEnum} from '../EnumAnnotationsNotNullable/Output/negative-enum';
-import { SByteEnum } from '../EnumAnnotationsNotNullable/Output/s-byte-enum';
-import { ShortEnum } from '../EnumAnnotationsNotNullable/Output/short-enum';
-import { SkippedEnum } from '../EnumAnnotationsNotNullable/Output/skipped-enum';
-import { SpecialNameEnum } from '../EnumAnnotationsNotNullable/Output/special-name-enum';
-import { StartingIndexEnum } from '../EnumAnnotationsNotNullable/Output/starting-index-enum';
-import { UIntEnum } from '../EnumAnnotationsNotNullable/Output/u-int-enum';
-import { ULongEnum } from '../EnumAnnotationsNotNullable/Output/u-long-enum';
-import { UShortEnum } from '../EnumAnnotationsNotNullable/Output/u-short-enum';
+import { ByteEnum } from '../EnumAnnotationsNullableDisabled/Output/byte-enum';
+import { DefaultEnum } from '../EnumAnnotationsNullableDisabled/Output/default-enum';
+import { IndexedEnum } from '../EnumAnnotationsNullableDisabled/Output/indexed-enum';
+import { IntEnum } from '../EnumAnnotationsNullableDisabled/Output/int-enum';
+import { LongEnum } from '../EnumAnnotationsNullableDisabled/Output/long-enum';
+import { NegativeEnum} from '../EnumAnnotationsNullableDisabled/Output/negative-enum';
+import { SByteEnum } from '../EnumAnnotationsNullableDisabled/Output/s-byte-enum';
+import { ShortEnum } from '../EnumAnnotationsNullableDisabled/Output/short-enum';
+import { SkippedEnum } from '../EnumAnnotationsNullableDisabled/Output/skipped-enum';
+import { SpecialNameEnum } from '../EnumAnnotationsNullableDisabled/Output/special-name-enum';
+import { StartingIndexEnum } from '../EnumAnnotationsNullableDisabled/Output/starting-index-enum';
+import { UIntEnum } from '../EnumAnnotationsNullableDisabled/Output/u-int-enum';
+import { ULongEnum } from '../EnumAnnotationsNullableDisabled/Output/u-long-enum';
+import { UShortEnum } from '../EnumAnnotationsNullableDisabled/Output/u-short-enum';
 
-import { Derived } from '../InheritanceAnnotationsNotNullable/Output/derived';
-import { DerivedFromAbstract } from '../InheritanceAnnotationsNotNullable/Output/derived-from-abstract';
-import { DerivedFromVirtual } from '../InheritanceAnnotationsNotNullable/Output/derived-from-virtual';
-import { Virtual } from '../InheritanceAnnotationsNotNullable/Output/virtual';
+import { Derived } from '../InheritanceAnnotationsNullableDisabled/Output/derived';
+import { DerivedFromAbstract } from '../InheritanceAnnotationsNullableDisabled/Output/derived-from-abstract';
+import { DerivedFromVirtual } from '../InheritanceAnnotationsNullableDisabled/Output/derived-from-virtual';
+import { Virtual } from '../InheritanceAnnotationsNullableDisabled/Output/virtual';
 
-import { SelfReferencingType } from '../SelfReferencingAnnotationsNullable/Output/self-referencing-type';
+import { SelfReferencingType } from '../SelfReferencingAnnotationsNullableEnabled/Output/self-referencing-type';
 
-import { StrictClass as StrictClassNotNullable } from '../StrictAnnotationsNotNullable/Output/strict-class';
-import { StrictInterface as StrictInterfaceNotNullable } from '../StrictAnnotationsNotNullable/Output/strict-interface';
+import { StrictClass as StrictClassNullableDisabled } from '../StrictAnnotationsNullableDisabled/Output/strict-class';
+import { StrictInterface as StrictInterfaceNullableDisabled } from '../StrictAnnotationsNullableDisabled/Output/strict-interface';
 
-import { StrictClass as StrictClassNullable } from '../StrictAnnotationsNullable/Output/strict-class';
-import { StrictInterface as StrictInterfaceNullable } from '../StrictAnnotationsNullable/Output/strict-interface';
+import { StrictClass as StrictClassNullable } from '../StrictAnnotationsNullableEnabled/Output/strict-class';
+import { StrictInterface as StrictInterfaceNullable } from '../StrictAnnotationsNullableEnabled/Output/strict-interface';
 
-import { GenericSubType as GenericSubTypeNotNullable } from '../TypesAnnotationsNotNullable/Output/generic-sub-type';
-import { SubType as SubTypeNotNullable } from '../TypesAnnotationsNotNullable/Output/sub-type';
-import { Types as TypesNotNullable } from '../TypesAnnotationsNotNullable/Output/types';
+import { GenericSubType as GenericSubTypeNullableDisabled } from '../TypesAnnotationsNullableDisabled/Output/generic-sub-type';
+import { SubType as SubTypeNullableDisabled } from '../TypesAnnotationsNullableDisabled/Output/sub-type';
+import { Types as TypesNullableDisabled } from '../TypesAnnotationsNullableDisabled/Output/types';
 
-import { GenericSubType as GenericSubTypeNullable } from '../TypesAnnotationsNullable/Output/generic-sub-type';
-import { SubType as SubTypeNullable } from '../TypesAnnotationsNullable/Output/sub-type';
-import { Types as TypesNullable } from '../TypesAnnotationsNullable/Output/types';
+import { GenericSubType as GenericSubTypeNullable } from '../TypesAnnotationsNullableEnabled/Output/generic-sub-type';
+import { SubType as SubTypeNullable } from '../TypesAnnotationsNullableEnabled/Output/sub-type';
+import { Types as TypesNullable } from '../TypesAnnotationsNullableEnabled/Output/types';
