11a5c2960433085bd1ae71b1f578d50cc423527f
Kai Melchior <kai@ky-programming.de>
2025-10-16 20:17:42 +0200

    fix: use Record type for typescript dictionaries fix: typescript dictionary with generic value type

--- TypeScript/Writers/TypeScriptDictionaryWriter.cs
+++ TypeScript/Writers/TypeScriptDictionaryWriter.cs
@@ -1,25 +1,23 @@
-﻿using System.Linq;
-using KY.Core;
+﻿using KY.Core;
 using KY.Generator.Output;
 using KY.Generator.Templates;
 using KY.Generator.Writers;
 
-namespace KY.Generator.TypeScript.Writers
+namespace KY.Generator.TypeScript.Writers;
+
+public class TypeScriptDictionaryWriter : IGenericTypeWriter
 {
-    public class TypeScriptDictionaryWriter : IGenericTypeWriter
+    public void Write(GenericTypeTemplate template, IOutputCache output)
     {
-        public void Write(GenericTypeTemplate template, IOutputCache output)
+        TypeTemplate keyType = template.Types.First();
+        TypeTemplate valueType = template.Types.Second();
+        if (keyType.Name is "string" or "number")
         {
-            TypeTemplate keyType = template.Types.First();
-            TypeTemplate valueType = template.Types.Second();
-            if (keyType.Name is "string" or "number")
-            {
-                output.Add($"{{ [key: {keyType.Name}]: {valueType.Name}; }}");
-            }
-            else
-            {
-                output.Add($"{{ /* unsupported type for key. Expected string or number. Got '{keyType.Name}'. */ }}");
-            }
+            output.Add("Record<").Add(keyType).Add(", ").Add(valueType).Add(">");
+        }
+        else
+        {
+            output.Add($"{{ /* unsupported type for key. Expected string or number. Got '{keyType.Name}'. */ }}");
         }
     }
 }
